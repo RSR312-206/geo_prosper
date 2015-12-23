c2c33ac2dc249647e2fe8f4202d9b643b6edeaac
RSR312-206 <roccosummer@gmail.com>
2015-12-23 14:57:47 -0800

    analyze_survey part 1 done

--- app/services/analyze_survey.rb
+++ app/services/analyze_survey.rb
@@ -5,33 +5,34 @@ class AnalyzeSurvey
   end
 
   def run
-
     JobWorker.perform_async(@industry_id, @job_id)
-    #all records by job_id which include city properties
+
+    #database calls for CitiesJobswages
     cities_jobs_wages = CitiesJobsWages.where(job_id: 5).joins(:city).all
     city_set = cities_jobs_wages.length
+    city_ids = cities_jobs_wages.map {|city_id| city_id.city_id }
+    # 40% weight
+    wages = cities_jobs_wages.sort_by {|wage| wage.wage }
 
-    cities_jobs_wages.each do |city_job_wage|
-      city =  city_job_wage.city_id
-      wages[city] = city_job_wage.wage
+    #database call for Industries
+    # 40% weight
+    industries = CitiesIndustries.where(city_id: city_ids, industry_id: @industry_id)
+    industry_rank = industries.sort_by { |ind| ind.rank }
 
-      industries[city] = city_job_wage.cities_industries.rank
-      open_housing[city] = city_job_wage.city.open_housing
-      unemployment[city] = city_job_wage.city.unemployment
-      weekly_hours_worked[city] = city_job_wage.city.weekly_hours_worked
-      commute_time[city] = city_job_wage.city.commute_time
-      housing_cost[city] = city_job_wage.city.housing_cost
 
-    end
+    #databse calls for Cities
+    cities = City.where(id: city_ids)
+    #30% weight
+    open_housing = cities.sort_by { |oh| oh.open_housing }
+    housing_cost = cities.sort_by { |hc| hc.housing_cost }
+    # 20% weight
+    unemployment = cities.sort_by { |u|  u.unemployment }
+    hours_worked = cities.sort_by { |hw| hw.hours_worked }
 
+    # 10% weight
+    commute_time = cities.sort_by { |ct| ct.commute_time }
   end
-
 end
-
-#for each city- find the place of each property in the hash and assign it a value based on the order.
-#so, if the wage is position #3 in the hash- the value of the wage property is 3
-
-
 
 # industry = CitiesIndustries.where(city_id: city_job_wage.city_id).pluck('industry_id').first
 # industries[city_job.city_id] = industry
