5b3ae1dcac611e87121875a143c981376e5bed04
RSR312-206 <roccosummer@gmail.com>
2015-12-26 15:14:58 -0800

    added static route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'static_pages#index'
   get '/welcome', to: 'static_pages#index'
+  get '/about', to: 'static_pages#about'
   resources :jobs, only: [:new, :create , :show]
 end
