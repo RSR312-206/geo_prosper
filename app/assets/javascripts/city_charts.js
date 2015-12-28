e50d82f413a54e053a94076aff9563a7bf21f909
RSR312-206 <roccosummer@gmail.com>
2015-12-27 21:43:01 -0800

    charts and initial design done

--- app/assets/javascripts/city_charts.js
+++ app/assets/javascripts/city_charts.js
@@ -24,7 +24,8 @@ $(function() {
 //salary chart constrcutror instatiation and type= Bar.
   new Chart(salary_chart).Bar(s_data, {
      scaleLabel:
-      function(label){return  '$' + label.value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");}
+      function(label){return  '$' + label.value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")},
+     scaleFontSize: 10
   });
 
 
@@ -48,7 +49,7 @@ $(function() {
         ]
     };
 
-  new Chart(industry_chart).Bar(industry_data);
+  new Chart(industry_chart).Bar(industry_data, {scaleFontSize: 10});
 
   //open housing
   var open_housing_chart = $("#open_housing").get(0).getContext("2d");
@@ -61,8 +62,8 @@ $(function() {
         datasets: [
             {
                 label: "",
-                fillColor: "RGBA(82, 87, 133, 1)",
-                strokeColor: "rgba(220,220,220,1)",
+                fillColor: "RGBA(125, 37, 51, 1)",
+                strokeColor: "RGBA(125, 37, 51, 1)",
                 pointColor: "rgba(220,220,220,1)",
                 pointStrokeColor: "#fff",
                 pointHighlightFill: "#fff",
@@ -72,7 +73,7 @@ $(function() {
         ]
     };
 
-  new Chart(open_housing_chart).Bar(open_housing_data, {scaleLabel: "<%=value + '%' %>" });
+  new Chart(open_housing_chart).Bar(open_housing_data, {scaleLabel: "<%=value + '%' %>", scaleFontSize: 10 });
 
   //unemployment Polar Area chart
   var unemployment_chart = $("#unemployment").get(0).getContext("2d");
@@ -83,7 +84,7 @@ $(function() {
     datasets: [
         {
             label: "",
-            fillColor: "rgba(220,220,220, 1)",
+            fillColor: "RGBA(79, 74, 72, 1)",
             strokeColor: "rgba(220,220,220,1)",
             pointColor: "rgba(220,220,220,1)",
             pointStrokeColor: "#fff",
@@ -95,7 +96,33 @@ $(function() {
   };
 
   new Chart(unemployment_chart).Bar(unemployment, {
-    pointDot: false, scaleLabel: "<%=value + '%' %>" });
+    pointDot: false, scaleLabel: "<%=value + '%' %>", scaleFontSize: 10 });
+
+// hours worked by city
+
+  var hours_worked_chart = $("#hours-worked").get(0).getContext("2d");
+  var new_hours_worked_chart = new Chart(hours_worked_chart)
+
+  var hours_data = {
+    labels: xaxis,
+    datasets: [
+      {
+          label: "My First dataset",
+          fillColor: "RGBA(83, 132, 122, 1)",
+          strokeColor: "rgba(220,220,220,1)",
+          pointColor: "rgba(220,220,220,1)",
+          pointStrokeColor: "#fff",
+          pointHighlightFill: "#fff",
+          pointHighlightStroke: "rgba(220,220,220,1)",
+          data: hours_worked_data
+      }
+    ]
+};
+
+  console.log(hours_worked_data)
+
+
+  new Chart(hours_worked_chart).Line(hours_data, {scaleFontSize: 10});
 
 
 });
