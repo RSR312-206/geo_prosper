01997c16af29575d99f49d6ac3392a8f5626e3f2
RSR312-206 <roccosummer@gmail.com>
2015-12-30 15:11:15 -0800

    student loan pmt now in salary charts. cleaned us chart file

--- app/assets/javascripts/city_charts.js
+++ app/assets/javascripts/city_charts.js
@@ -1,16 +1,16 @@
 $(function() {
 
-//salary charts - comparing all cities by salary
+//salary chart
   var salary_chart = $("#salary_chart").get(0).getContext("2d");
   var new_salary_chart = new Chart(salary_chart);
 
-//salary chart colors and data
   var s_data = {
         labels: xaxis,
         datasets: [
             {
                 label: "",
-                fillColor: "RGBA(82, 87, 133, 1)",
+                type: "bar",
+                fillColor: "RGBA(36, 42, 59, 1)",
                 strokeColor: "rgba(220,220,220,1)",
                 pointColor: "rgba(220,220,220,1)",
                 pointStrokeColor: "#fff",
@@ -18,19 +18,28 @@ $(function() {
                 pointHighlightStroke: "rgba(220,220,220,1)",
                 data: salary_yaxis
             },
+            {
+                label: "With Student Loan Payment",
+                type: "bar",
+                fillColor: "RGBA(204, 177, 152, 1)",
+                strokeColor: "rgba(220,220,220,1)",
+                pointColor: "rgba(220,220,220,1)",
+                pointStrokeColor: "#fff",
+                pointHighlightFill: "#fff",
+                pointHighlightStroke: "rgba(220,220,220,1)",
+                data: student_loan_pmt
+            }
         ]
     };
 
-//salary chart constrcutror instatiation and type= Bar.
-  new Chart(salary_chart).Bar(s_data, {
+  new Chart(salary_chart).Overlay(s_data, {
      scaleLabel:
       function(label){return  '$' + label.value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") },
      scaleFontSize: 10,
      scaleBeginAtZero : false
-  });
+  })
 
-
-  //open housing
+//open housing
   var open_housing_data = {
         labels: xaxis,
         datasets: [
@@ -56,11 +65,12 @@ $(function() {
             }
         ]
     };
+
   var new_open_housing = new
 
   Chart($("#open_housing").get(0).getContext("2d")).Overlay(open_housing_data, {scaleLabel: "<%=value + '%' %>", scaleFontSize: 10, scaleBeginAtZero : false });
 
-  //unemployment Polar Area chart
+//unemployment Polar Area chart
   var unemployment_chart = $("#unemployment").get(0).getContext("2d");
   var new_unemployment_chart = new Chart(unemployment_chart);
 
@@ -94,7 +104,6 @@ $(function() {
     pointDot: true, scaleLabel: "<%=value + '%' %>", scaleFontSize: 10 });
 
 // hours worked by city
-
   var hours_worked_chart = $("#hours-worked").get(0).getContext("2d");
   var new_hours_worked_chart = new Chart(hours_worked_chart)
 
@@ -116,7 +125,8 @@ $(function() {
 
   new Chart(hours_worked_chart).Line(hours_data, {scaleFontSize: 10 });
 
-  //industry list
+//industry list
+
   var list_options = {
     valueNames: [ 'name', 'rank' ]
   };
@@ -130,7 +140,7 @@ $(function() {
             {
                 label: "",
                 type: "line",
-                fillColor: "RGBA(125, 37, 51, 1)",
+                fillColor: "RGBA(250, 203, 112, 1)",
                 strokeColor: "RGBA(125, 37, 51, 1)",
                 pointColor: "rgba(220,220,220,1)",
                 pointStrokeColor: "#fff",
@@ -141,7 +151,7 @@ $(function() {
             {
                label: "National Average",
                type: "line",
-               fillColor: "rgba(151,187,205,0.5)",
+               fillColor: "RGBA(89, 89, 95, )",
                strokeColor: "rgba(151,187,205,0.8)",
                highlightFill: "rgba(151,187,205,0.75)",
                highlightStroke: "rgba(151,187,205,1)",
